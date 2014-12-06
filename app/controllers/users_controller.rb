355dbb132d426c8ce874700be4ffbbbb858fffab
Ryan MacGillivray <rmacg89@gmail.com>
2014-12-06 23:06:45 +0000

    remove double quote strings since there is no interpolation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Account created, welcome!"
+      flash[:success] = 'Account created, welcome!'
       redirect_to @user
     else
       render 'new'
