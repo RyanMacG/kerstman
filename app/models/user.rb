9e73584b310f169b783ee99ec76297d14cbc7464
Ryan MacGillivray <rmacg89@gmail.com>
2014-12-06 22:14:51 +0000

    refactor user email downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_validation { email.downcase }
   validates_presence_of :name
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates_format_of :email, with: VALID_EMAIL_REGEX, message: 'Must be a valid email address'
