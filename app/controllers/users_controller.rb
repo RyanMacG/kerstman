4160546b8c7da02e8fdc6b2affa1a5282dbffa60
Ryan MacGillivray <rmacg89@gmail.com>
2014-12-06 22:43:05 +0000

    set up create and show user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,30 @@
 class UsersController < ApplicationController
+
   def new
     @page_title = 'Sign Up'
     @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @page_title = @user.name
+  end
+
+  # CRUD methods
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Account created, welcome!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+    :password_confirmation)
   end
 end
