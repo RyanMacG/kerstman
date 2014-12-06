d68468ded0ba313954efc853e7b9b4f60d37ec68
Ryan MacGillivray <rmacg89@gmail.com>
2014-12-06 22:11:49 +0000

    add password validations to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates_format_of :email, with: VALID_EMAIL_REGEX, message: 'Must be a valid email address'
   validates_uniqueness_of :email, case_sensitive: false, message: 'Sorry this email is already taken'
+  validates :password, length: { minimum: 6 }
+  validates_confirmation_of :password,
+  if: lambda { |m| m.password.present? }
+  has_secure_password
 end
