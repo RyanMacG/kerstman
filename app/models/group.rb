242dd18f96fdf221cd1c56cb580b070eec837aee
Ryan MacGillivray <rmacg89@gmail.com>
2014-12-07 12:44:53 +0000

    add associations for participants to groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ActiveRecord::Base
   belongs_to :user
+  has_many :participants, dependent: :destroy
   default_scope -> { order('created_at DESC') }
   validates_presence_of :user_id, :name
 end
