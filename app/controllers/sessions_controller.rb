e64dd1dc10031fd58880c59682f3ac1532ddee88
Ryan MacGillivray <rmacg89@gmail.com>
2014-12-07 10:14:11 +0000

    use session_store on the sign in action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session[:email].downcase)
     if user && user.authenticate(session[:password])
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       @page_title = 'Sign in'
