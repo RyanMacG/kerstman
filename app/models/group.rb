14b48e2fbc22b067b93f7f363a4ec5648be347a9
Ryan MacGillivray <rmacg89@gmail.com>
2014-12-07 19:29:06 +0000

    add matched method to try and make sure everyone has a partner

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,15 @@ class Group < ActiveRecord::Base
   has_many :participants, dependent: :destroy
   default_scope -> { order('created_at DESC') }
   validates_presence_of :user_id, :name
+
+  def self.matched
+    binding.pry
+    p 'foo'
+    participants.each do |part|
+      if part.giftee.blank?
+        matched = false
+      end
+      return matched
+    end
+  end
 end
