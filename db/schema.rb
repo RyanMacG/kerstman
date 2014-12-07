6cc6c551242bebb6f158b888743e3da3eae96e02
Ryan MacGillivray <rmacg89@gmail.com>
2014-12-07 10:25:06 +0000

    add groups table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207000557) do
+ActiveRecord::Schema.define(version: 20141207102128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.string   "limit"
+    t.string   "year"
+    t.boolean  "matched"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "groups", ["user_id", "created_at"], name: "index_groups_on_user_id_and_created_at", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
