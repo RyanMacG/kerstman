47373f223997cc6468dc6780cc0f325968357e1e
Ryan MacGillivray <rmacg89@gmail.com>
2014-12-07 19:29:19 +0000

    initial buggy to fuck matching method

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :signed_in_user, only: [:create, :destroy, :index, :show]
-  before_action :correct_user, only: [:destroy, :show]
+  before_action :signed_in_user, only: [:create, :destroy, :index, :show, :match]
+  before_action :correct_user, only: [:destroy, :show, :match]
 
   def index
   end
@@ -27,6 +27,25 @@ class GroupsController < ApplicationController
     @participants = @group.participants.order('created_at ASC')
                           .paginate(per_page: 15, page: (params[:page]))
     @page_title = @group.name
+  end
+
+  def match
+    @group.participants.each do |participant|
+      potential_giftees = Participant.matchable(group: @group, part: participant)
+      giftee = potential_giftees.sample
+      if giftee.blank? && !@group.matched
+        @group.participants.update_all(giftee_id: nil, matched: false)
+        match
+      end
+      participant.giftee_id = giftee.id
+      giftee.matched = true
+      participant.save && giftee.save
+    end
+    p 'done'
+    if @group.matched
+      p 'success'
+    end
+    redirect_to group_path(@group)
   end
 
   private
